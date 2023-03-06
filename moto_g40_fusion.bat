a6d69d67efd3bb6b209e29a9ba4abe4201cb78fb
hex-willer <75025502+hex-willer@users.noreply.github.com>
2023-03-07 02:05:40 +0530

    moto g40 fusion

---
+++ moto_g40_fusion.bat
@@ -0,0 +1,26 @@
+adb shell pm uninstall --user 0 com.facebook.katana
+adb shell pm uninstall --user 0 com.facebook.system
+adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.facebook.services
+adb shell pm uninstall --user 0 com.google.android.youtube
+adb shell pm uninstall --user 0 com.google.android.apps.youtube.music
+adb shell pm uninstall --user 0 com.google.android.apps.youtube.music.setupwizard
+adb shell pm uninstall --user 0 com.payjoy.access
+adb shell pm uninstall --user 0 com.motorola.timeweatherwidget
+adb shell pm uninstall --user 0 com.motorola.mototour
+adb shell pm uninstall --user 0 com.motorola.discovery
+adb shell pm uninstall --user 0 com.google.android.feedback
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.google.android.apps.podcasts
+adb shell pm uninstall --user 0 com.google.ar.core
+adb shell pm uninstall --user 0 com.google.android.apps.subscriptions.red
+adb shell pm uninstall --user 0 com.google.android.gms.location.history
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall --user 0 com.motorola.gamemode
+adb shell pm uninstall --user 0 com.google.android.apps.fitness
+adb shell pm uninstall --user 0 com.motorola.demo
+adb shell pm uninstall --user 0 com.motorola.brapps
+adb shell pm uninstall --user 0 com.google.android.apps.googleassistant
+
+
+
